34638349fe232f0a9b01db535515fc20d31c1cd5
Yushi Canastra <ycanastra@gmail.com>
2018-05-29 21:34:22 -0500

    use destructuring, one liner

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const canvas = document.querySelector('#main-canvas');
 const ctx = canvas.getContext('2d');
 
 const imgData = ctx.createImageData(160, 144);
-const data = imgData.data;
+const { data } = imgData;
 
 gameBoy.on('screen-update', () => {
   for (let i = 0, len = 160 * 144 * 4; i < len; i += 4) {
